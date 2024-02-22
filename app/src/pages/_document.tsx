ec7d2ad1364f8c520ba5d9ce6b9b1b96dc7d4879
dahoon06 <dahoon226@naver.com>
2024-02-22 23:55:27 +0900

    chore: 파비콘 추가

--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link rel="stylesheet" as="style"
               href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable-dynamic-subset.min.css"/>
-
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico"/>
       </Head>
       <body>
