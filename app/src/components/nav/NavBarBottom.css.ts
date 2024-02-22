f0fd7aa7f486b36a84a142006602a55f5dd0c236
dahoon06 <dahoon226@naver.com>
2024-02-22 16:26:41 +0900

    feat: bottom navigation posting 버튼 추가

--- app/src/components/nav/NavBarBottom.css.ts
+++ app/src/components/nav/NavBarBottom.css.ts
@@ -8,8 +8,6 @@ export const navBarBottomLayout = style({
   maxWidth: 428,
   display: 'flex',
   alignItems: 'center',
-  backgroundColor: vars.colors.white000,
-  borderTop: '1px solid #ededed',
   transition: 'box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms',
   height: 64,
   zIndex: 1100,
@@ -19,6 +17,7 @@ export const navBarBottomContainer = style({
   height: '100%',
   display: 'flex',
   alignItems: 'center',
+  justifyContent: 'space-between'
 })
 
 export const navBarBottomLists = style({
@@ -34,4 +33,47 @@ export const navBarBottomItems = style({
   flexDirection: 'column',
   alignItems: "center",
   justifyContent: 'center'
+})
+
+export const navBarLeftBox = style({
+  display: "flex",
+  backgroundColor: vars.colors.white000,
+  width: "50%",
+  height: "100%",
+  borderRadius: " 0 45px 0 0",
+  borderTop: '1px solid #ededed',
+  justifyContent: 'center',
+  gap: 30
+})
+
+export const navBarCenterBox = style({
+  position: 'relative',
+  height: '100%',
+  right: 30,
+  bottom: 16,
+  backgroundColor: vars.colors.white000,
+});
+
+export const navBarCenterButton = style({
+  position: 'absolute',
+  backgroundColor: vars.colors.primary,
+  width: 60,
+  height: '100%',
+  borderRadius: '50%',
+  display: "flex",
+  alignItems: 'center',
+  justifyContent: 'center',
+  boxShadow: "0px 1px 2px 1px rgba(0,0,0,0.2)",
+  zIndex: 99
+})
+
+export const navBarRightBox = style({
+  display: "flex",
+  backgroundColor: vars.colors.white000,
+  width: "50%",
+  height: "100%",
+  borderRadius: "45px 0 0 0",
+  borderTop: '1px solid #ededed',
+  justifyContent: 'center',
+  gap: 30
 })
--- app/src/components/nav/NavBarBottom.tsx
+++ app/src/components/nav/NavBarBottom.tsx
@@ -1,48 +1,45 @@
 import * as styles from './NavBarBottom.css';
-import {RiEditLine, RiHome5Line, RiMessage3Line, RiStarSLine, RiUserLine} from "react-icons/ri";
+import {RiHome5Line, RiMessage3Line, RiPencilFill, RiStarSLine, RiUserLine} from "react-icons/ri";
 import {Typography} from "@components/common/typography/Typography";
 
 export const NavBarBottom = () => {
   return (
     <nav className={styles.navBarBottomLayout}>
-      <div className={styles.navBarBottomContainer}>
-        <ul className={styles.navBarBottomLists}>
-          <li className={styles.navBarBottomItems}>
+      <ul className={styles.navBarBottomContainer}>
+        <li className={styles.navBarLeftBox}>
+          <button type={'button'} className={styles.navBarBottomItems}>
             <RiHome5Line size={22} color={'#646464'}/>
             <Typography as={'span'} color={'gray400'} fontWeight={500} fontSize={12}>
               홈
             </Typography>
-          </li>
-          <li className={styles.navBarBottomItems}>
-            <RiEditLine size={22} color={'#646464'}/>
-            <Typography as={'span'} color={'gray400'} fontWeight={500} fontSize={12}>
-              탐색
-            </Typography>
-
-          </li>
-          <li className={styles.navBarBottomItems}>
+          </button>
+          <button type={'button'} className={styles.navBarBottomItems}>
             <RiMessage3Line size={22} color={'#646464'}/>
             <Typography as={'span'} color={'gray400'} fontWeight={500} fontSize={12}>
-              선물관
+              메세지
             </Typography>
-
-          </li>
-          <li className={styles.navBarBottomItems}>
+          </button>
+        </li>
+        <li className={styles.navBarCenterBox}>
+          <button type={'button'} aria-label={'post_register_button'} className={styles.navBarCenterButton}>
+            <RiPencilFill size={22} color={'#fff'}/>
+          </button>
+        </li>
+        <li className={styles.navBarRightBox}>
+          <button type={'button'} className={styles.navBarBottomItems}>
             <RiStarSLine size={22} color={'#646464'}/>
             <Typography as={'span'} color={'gray400'} fontWeight={500} fontSize={12}>
-              기획전
+              즐겨찾기
             </Typography>
-
-          </li>
-          <li className={styles.navBarBottomItems}>
+          </button>
+          <button type={'button'} className={styles.navBarBottomItems}>
             <RiUserLine size={22} color={'#646464'}/>
             <Typography as={'span'} color={'gray400'} fontWeight={500} fontSize={12}>
               마이
             </Typography>
-
-          </li>
-        </ul>
-      </div>
+          </button>
+        </li>
+      </ul>
     </nav>
   )
 }
