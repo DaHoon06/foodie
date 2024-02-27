b30bdd6b384eeb29361a0ae2df58221fa5c4f5f2
dahoon06 <dahoon226@naver.com>
2024-02-27 15:08:57 +0900

    feat: useInfinityScroll 추가

--- app/src/components/lists/StoreLists.tsx
+++ app/src/components/lists/StoreLists.tsx
@@ -1,7 +1,11 @@
-import {ReactElement, useEffect} from "react";
+import {ReactElement} from "react";
 import * as styles from './StoreLists.css';
 import {StoreCard, StoreCardItem} from "@components/cards/StoreCard";
-import {prefetchingRestaurantList, useRestaurantQuery} from "@services/queries/listQuery";
+import {useInfiniteQuery} from "@tanstack/react-query";
+import {AxiosError, AxiosResponse} from "axios";
+import {queryKeys} from "@services/keys/queryKeys";
+import {getRestaurantListsApi} from "@services/apis/restaurant";
+import {useIntersectionObserver} from "@hooks/useIntersectionObserver";
 
 interface Props {
   sort: string;
@@ -9,28 +13,58 @@ interface Props {
 
 export const StoreLists = (props: Props): ReactElement => {
   const {sort} = props;
-  const {data: listQueryData, isLoading} = useRestaurantQuery(sort);
-  useEffect(() => {
-    prefetchingRestaurantList().then();
-  }, []);
+
+  const {
+    data: listQueryData,
+    isLoading,
+    fetchNextPage,
+    hasNextPage
+  } = useInfiniteQuery<AxiosResponse, AxiosError, ResponseReturnValue<{ data: StoreCardItem[] }>>(
+    [queryKeys.lists.restaurantLists],
+    ({pageParam = 1}) => getRestaurantListsApi(sort, {pageParam}),
+    {
+      getNextPageParam: (lastPage, allPages) => {
+        const nextPage = allPages.length + 1;
+        return lastPage.data.length === 0 ? undefined : nextPage;
+      },
+      select: (data) => ({
+        pages: data?.pages.flatMap((page) => page.data),
+        pageParams: data.pageParams
+      })
+    }
+  )
+
+  // const {data: listQueryData, isLoading} = useRestaurantQuery(sort);
+  // useEffect(() => {
+  //   prefetchingRestaurantList().then();
+  // }, []);
+
+  const {setTarget} = useIntersectionObserver({
+    hasNextPage,
+    fetchNextPage,
+  });
 
   if (isLoading) return <div>...isLoading</div>
   //todo: 데이터가 없을 경우에 대한 처리 필요
   if (!listQueryData) return <div>데이터가 존재하지 않습니다.</div>
 
   return (
-    <article className={styles.storeListsLayout}>
-      <div className={styles.storeListsContainer}>
-        {listQueryData.data.data.map((store: StoreCardItem, index: number) => {
-          return (
-            <div key={index}>
-              <StoreCard
-                items={store}
-              />
-            </div>
-          )
-        })}
-      </div>
-    </article>
+    <>
+      <article className={styles.storeListsLayout}>
+        <div className={styles.storeListsContainer}>
+          {listQueryData.data.data.map((store: StoreCardItem, index: number) => {
+            return (
+              <div key={index}>
+                <StoreCard
+                  items={store}
+                />
+              </div>
+            )
+          })}
+        </div>
+      </article>
+      <div ref={setTarget}/>
+    </>
+
   )
 }
