390968867c1413e3b457bfff191510c23397c1c2
dahoon06 <dahoon226@naver.com>
2024-03-09 23:23:08 +0900

    chore: proxy url 설정

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -5,6 +5,14 @@ const withVanillaExtract = createVanillaExtractPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*/",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
+      },
+    ];
+  },
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
