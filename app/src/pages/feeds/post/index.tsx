d8323f0585e29af55cd2bf2fa6af2063c8cbb678
dahoon06 <dahoon226@naver.com>
2024-03-12 00:16:38 +0900

    feat: Post Container 분리

--- app/src/pages/feeds/post/index.tsx
+++ app/src/pages/feeds/post/index.tsx
@@ -2,67 +2,41 @@ import FlexBox from "@components/common/headless/flex-box/FlexBox";
 import { Typography } from "@components/common/typography/Typography";
 import { BasicLayout } from "@layouts/BasicLayout";
 import { NextPage } from "next";
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
 import * as styles from "@styles/pages/feeds/PostPage.css";
 import { FeedPost } from "@components/feeds/FeedPost";
 import { Button } from "@components/buttons";
 import { Avatar } from "@components/ui";
 import { useRouter } from "next/router";
-import { FiCamera, FiMapPin } from "react-icons/fi";
+import { FiMapPin } from "react-icons/fi";
+import { FileUploadButton } from "@components/buttons/FileUploadButton";
+import Image from "next/image";
+import { FeedPostContainer } from "@containers/PostContainer";
 
 const FeedPostPage: NextPage = (): ReactElement => {
   const router = useRouter();
+  
 
   return (
     <BasicLayout>
       <FlexBox
-          height={56}
-          justifyContent="space-between"
-          className={styles.feedListsTitle}
-          direction="row"
-        >
-          <button type="button" onClick={() => router.back()}>
-            <Typography color="primary" as="span" fontSize={14}>
-              취소
-            </Typography>
-          </button>
-          <Button type="submit">
-            <Typography as="span" fontSize={12} color={"white000"}>
-              포스팅
-            </Typography>
-          </Button>
-        </FlexBox>
-      <div className={styles.postLayout}>
-        <FlexBox
-          className={styles.postBodyContainer}
-          direction="row"
-          alignItems="flex-center"
-          justifyContent="flex-start"
-        >
-          <Avatar alt={"dahoon"} src={"/images/dh.png"} />
-          <FeedPost />
-        </FlexBox>
-
-        <FlexBox
-          direction="row"
-          justifyContent="space-between"
-          className={styles.postOptionContainer}
-        >
-          <FlexBox direction="row" justifyContent="flex-start" gap={4}>
-            <FiCamera color={"#FF7101"} />
-            <Typography color="primary" as="span" fontSize={14}>
-              사진
-            </Typography>
-          </FlexBox>
-          <FlexBox direction="row" justifyContent="flex-end" gap={4}>
-            <FiMapPin color={"#FF7101"} />
-            <Typography color="primary" as="span" fontSize={14}>
-              장소
-            </Typography>
-          </FlexBox>
-        </FlexBox>
-        <div className={styles.imagesContainer}></div>
-      </div>
+        height={56}
+        justifyContent="space-between"
+        className={styles.feedListsTitle}
+        direction="row"
+      >
+        <button type="button" onClick={() => router.back()}>
+          <Typography color="primary" as="span" fontSize={14}>
+            취소
+          </Typography>
+        </button>
+        <Button type="submit">
+          <Typography as="span" fontSize={12} color={"white000"}>
+            포스팅
+          </Typography>
+        </Button>
+      </FlexBox>
+      <FeedPostContainer />
     </BasicLayout>
   );
 };
--- app/src/styles/pages/feeds/PostPage.css.ts
+++ app/src/styles/pages/feeds/PostPage.css.ts
@@ -21,26 +21,3 @@ export const feedListsLayout = style({
   padding: "8px 0",
 });
 
-export const postLayout = style({
-  backgroundColor: vars.colors.white000,
-  width: "100%",
-  height: "100%",
-});
-
-export const postBodyContainer = style({
-  backgroundColor: vars.colors.white000,
-  width: "100%",
-  padding: "1em",
-  borderBottom: "1px solid #f5f5f5",
-});
-
-export const postOptionContainer = style({
-  height: "60px !important",
-  padding: "0.5em 1.25em",
-  backgroundColor: vars.colors.white000,
-});
-
-export const imagesContainer = style({
-  backgroundColor: vars.colors.white000,
-  width: "100%",
-});
