05fdbee7050e63745ab175713b8b11bf9437da45
dahoon06 <dahoon226@naver.com>
2024-03-15 21:54:56 +0900

    test: 너비 구하기 로직 작성 (그린)

--- app/src/__tests__/utils.function.tests.ts
+++ app/src/__tests__/utils.function.tests.ts
@@ -1,4 +1,4 @@
-import { widthCaculation } from "@components/ui/rating/star";
+import {widthCalculation} from "@components/ui/rating/star";
 
 /**
  * @description 별점 계산
@@ -8,60 +8,60 @@ import { widthCaculation } from "@components/ui/rating/star";
  */
 
 describe("평균 점수를 가지고 너비 구하기", () => {
-  let widthCaculationMockfn: jest.Mock;
+  let widthCalculationMockFn: jest.Mock;
 
   beforeEach(() => {
-    widthCaculationMockfn = jest.fn(widthCaculation);
+    widthCalculationMockFn = jest.fn(widthCalculation);
   });
 
   test("0 ~ 0.5 점일 경우 별 0.5칸 반환 : 10%", () => {
     const expected = "10";
     const average = 0.2;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("0.6 ~ 1 점일 경우 별 1칸 반환 : 20%", () => {
     const expected = "20";
     const average = 1;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("1.1 ~ 1.5 점일 경우 별 1.5칸 반환 : 30%", () => {
     const expected = "30";
     const average = 1.1;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("1.6 ~ 2 점일 경우 별 2칸 반환 : 40%", () => {
     const expected = "40";
     const average = 1.8;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("2.1 ~ 2.5 점일 경우 별 2.5칸 반환 : 50%", () => {
     const expected = "50";
     const average = 2.2;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("2.6 ~ 3 점일 경우 별 3칸 반환 : 60%", () => {
     const expected = "60";
     const average = 2.9;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("3.1 ~ 4 점일 경우 별 3.5칸 반환 : 70%", () => {
     const expected = "70";
     const average = 4;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("4.1 ~ 4.5 점일 경우 별 4칸 반환 : 80%", () => {
     const expected = "80";
     const average = 4.2;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("4.6 ~ 4.9 점일 경우 별 4.5칸 반환 : 90%", () => {
     const expected = "90";
     const average = 4.8;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
   test("5 점일 경우 별 5칸 반환 : 100%", () => {
     const expected = "100";
     const average = 5;
-    expect(widthCaculationMockfn(average)).toEqual(expected);
+    expect(widthCalculationMockFn(average)).toEqual(expected);
   });
 });
--- app/src/components/ui/rating/star.tsx
+++ app/src/components/ui/rating/star.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import {ReactElement} from "react";
 import * as styles from "./star.css";
 
 interface StarRatingProps {
@@ -7,21 +7,44 @@ interface StarRatingProps {
 
 /**
  * @description 너비 구하기
- * @param average 
+ * @param average
  */
-export const widthCaculation = (average: number) => {
-
+export const widthCalculation = (average: number) => {
+  if (average >= 0 && average <= 0.5) {
+    return "10";
+  } else if (average > 0.5 && average <= 1) {
+    return "20";
+  } else if (average > 1 && average <= 1.5) {
+    return "30";
+  } else if (average > 1.5 && average <= 2) {
+    return "40";
+  } else if (average > 2 && average <= 2.5) {
+    return "50";
+  } else if (average > 2.5 && average <= 3) {
+    return "60";
+  } else if (average > 3 && average <= 4) {
+    return "70";
+  } else if (average > 4 && average <= 4.5) {
+    return "80";
+  } else if (average > 4.5 && average <= 4.9) {
+    return "90";
+  } else if (average === 5) {
+    return "100";
+  } else {
+    return '100';
+  }
 }
 
 export const StarRating = (props: StarRatingProps): ReactElement => {
-  const { average } = props;
+  const {average} = props;
+  const width = widthCalculation(average);
 
-  
+
   return (
     <div className={styles.starRatings}>
       <div
         className={styles.starRatingsFill}
-        style={{ width: `${average * 10}%` }}
+        style={{width: `${width}%`}}
       >
         <span>★</span>
         <span>★</span>
