906bbc28295d46423f9e91f218e572d8a2db4735
dahoon06 <dahoon226@naver.com>
2024-03-15 23:06:08 +0900

    fix: mocking url 수정

--- app/src/mocks/handlers/restaurant/handler.ts
+++ app/src/mocks/handlers/restaurant/handler.ts
@@ -6,18 +6,18 @@ import {StoreCardItem} from "@components/cards/StoreCard";
 const DOMAIN = '/api/restaurant'
 
 export const getRestaurantListsApiMocking = http.get(
-  `${DOMAIN}`,
-  ({ request }) => {
-    const url = new URL(request.url);
-    const page =  +url.searchParams.get('page') || 1;
+  `${DOMAIN}/list`,
+  ({request}) => {
+      const url = new URL(request.url);
+      const page = +url.searchParams.get('page') || 1;
 
-    const limit = 20;
-    // exception error
-    const error = false;
-    if (error) return new HttpResponse(null, {status: 404});
+      const limit = 20;
+      // exception error
+      const error = false;
+      if (error) return new HttpResponse(null, {status: 404});
 
-    const paginationData = restaurantListMockData.slice((page - 1) * limit, page * limit);
-    const data = responseConversion<StoreCardItem[]>(
-      paginationData);
-    return HttpResponse.json({...data});
+      const paginationData = restaurantListMockData.slice((page - 1) * limit, page * limit);
+      const data = responseConversion<StoreCardItem[]>(
+        paginationData);
+      return HttpResponse.json({...data});
   })
