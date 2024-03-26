306be4c16d56284364aea8d62286bd3d99323aed
dahoon06 <dahoon226@naver.com>
2024-03-26 22:30:53 +0900

    chore: db 변경 - mongodb -> mysql

--- server/src/config/db/db.module.ts
+++ server/src/config/db/db.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongoConnector } from '@config/db/index';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CONFIG_OPTION, TYPEORM_OPTION } from './config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 const mongoConnector = new MongoConnector();
 
 @Module({
-  imports: [mongoConnector.GoFoodieDatabase],
+  imports: [
+    mongoConnector.GoFoodieDatabase,
+    ConfigModule.forRoot(CONFIG_OPTION()),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: TYPEORM_OPTION,
+    }),
+  ],
 })
 export class DataBaseModule {}
